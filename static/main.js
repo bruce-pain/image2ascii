1eddf1a01eb84c087de78e6143e8e5ba4f9136fc
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-09-09 13:43:59 +0100

    feat: new background color, border line, improved quality

--- static/main.js
+++ static/main.js
@@ -3,6 +3,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 
 	const statusBar = document.getElementById('status-bar');
 	const asciiOutput = document.getElementById('asciiOutput');
+	const displayArea = document.getElementById('display');
 
 	const fileInput = document.getElementById('fileInput');
 	const formData = new FormData();
@@ -15,6 +16,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 	let imageWidth;
 
 	statusBar.innerHTML = "generating..."
+	displayArea.style.border = "0px";
 	asciiOutput.innerHTML = ""
 
 	if (fileInput.files[0] == null) {
@@ -23,13 +25,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 		formData.append('image_file', fileInput.files[0]);
 
 		try {
-			if (screenWidth < 500) {
-				imageWidth = 150
-			} else {
-				imageWidth = 200
-			}
-
-
+			imageWidth = 400
 			const response = await fetch(`/upload?width=${imageWidth}&character_set=${selectedCharSet}&is_colored=${addColor}`, {
 				method: 'POST',
 				body: formData
@@ -38,6 +34,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 			const data = await response.json();
 			const result = data["result"];
 			statusBar.innerHTML = "done!";
+			displayArea.style.border = "1px solid #FCFCFC";
 
 			if (addColor == true) {
 				asciiOutput.innerHTML = result;
